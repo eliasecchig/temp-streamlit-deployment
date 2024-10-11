6ef0d9c55226da9453a8673c8a1cf45ae1d35941
Your Name <eliasecchi@google.com>
2024-10-11 12:07:07 +0200

    fix linter python

--- app/server.py
+++ app/server.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=W0718,C0411
-# ruff: noqa: I001
+# noqa: E266, W0718, C0411, I001
 
 import json
 import logging
